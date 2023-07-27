532f1be05c4206bb524a2c545583bde996f23fdc
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-07-27 15:52:41 +0100

    first commit

---
+++ src/main/java/com/qa/demo/MorseTranslator.java
@@ -0,0 +1,66 @@
+package com.qa.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MorseTranslator {
+
+
+
+    public MorseTranslator() {
+    }
+
+    private static Map<String, String> morseCode = new HashMap<>();
+    static {
+        morseCode.put(".-", "A");
+        morseCode.put("-...", "B");
+        morseCode.put("-.-.", "C");
+        morseCode.put("-..", "D");
+        morseCode.put(".", "E");
+        morseCode.put("..-.", "F");
+        morseCode.put("--.", "G");
+        morseCode.put("....", "H");
+        morseCode.put("..", "I");
+        morseCode.put(".---", "J");
+        morseCode.put("-.-", "K");
+        morseCode.put(".-..", "L");
+        morseCode.put("--", "M");
+        morseCode.put("-.", "N");
+        morseCode.put("---", "O");
+        morseCode.put(".--.", "P");
+        morseCode.put("--.-", "Q");
+        morseCode.put(".-.", "R");
+        morseCode.put("...", "S");
+        morseCode.put("-", "T");
+        morseCode.put("..-", "U");
+        morseCode.put("...-", "V");
+        morseCode.put(".--", "W");
+        morseCode.put("-..-", "X");
+        morseCode.put("-.--", "Y");
+        morseCode.put("--..", "Z");
+
+        morseCode.put(".----", "1");
+        morseCode.put("..---", "2");
+        morseCode.put("...--", "3");
+        morseCode.put("....-", "4");
+        morseCode.put(".....", "5");
+        morseCode.put("-....", "6");
+        morseCode.put("--...", "7");
+        morseCode.put("---..", "8");
+        morseCode.put("----.", "9");
+        morseCode.put("-----", "0");
+        morseCode.put("/", " ");
+    }
+
+    static String translate(String toTranslate) {
+        StringBuilder translated = new StringBuilder();
+        String[] characters = toTranslate.split(" ");
+        for (String morse : characters) {
+            translated.append(morseCode.get(morse));
+        }
+
+        return translated.toString();
+    }
+
+
+}
